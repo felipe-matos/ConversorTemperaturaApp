7cec1ec48c341e68b60ba1da122cf5c38a6b5372
Felipe Matos <felipematosapps@gmail.com>
2024-07-17 19:40:43 -0300

    segundo commit

--- app/src/main/java/com/conversortemperaturaapp/MainActivity.kt
+++ app/src/main/java/com/conversortemperaturaapp/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.conversortemperaturaapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.conversortemperaturaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val  binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.buttonConverter.setOnClickListener {
+            val clesius =  binding.editCelsius.text.toString().toDouble()
+            val fahrenheit = clesius * 1.8 + 32
+
+            binding.textResultado.text = "$fahrenheit ºF"
+        }
 
     }
 }
